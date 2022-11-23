2bbfb20793de74d893811c1acc6151c9901b2c97
Valery Gridnev <valer.gr@gmail.com>
2022-11-24 07:42:02 +1100

    external POI

--- webclient/terra3d.js
+++ webclient/terra3d.js
@@ -1,23 +1,42 @@
 import './terra3d.css';
 import * as env from './env.json';
+import * as places from './terra3dplaces.json';
 import Procedural from 'procedural-gl';
 
-// POI
-const kyrg = [74.57950579031711, 42.51248314829303]
-const khanTengri = [80.17411914133028, 42.213405765504476]
-const katoomba = [150.3120553998699, -33.73196775624329]
-const uluru = [131.03388514743847, -25.34584297139171]
-const mtDenali = [-151.00726915968875,63.069268194834244]
-const pikPobedy = [80.129257551509, 42.03767896555761]
-const mtEverest = [86.9251465845193, 27.98955908635046]
-const mtOlympus = [22.35011553189942, 40.08838447876729]
-const mtKilimanjaro = [37.35554126906301,-3.065881717083569]
-const cordilleraBlanca = [-77.5800702637765,-9.169719296932207]
-const grandCanyon = [-112.09523569822798,36.10031704536186]
-const oahuHawaii = [-157.80960937978762,21.26148763859345]
-const mtFuji = [138.73121113691982,35.363529199406074]
-const mtTaranaki = [174.06318456572237,-39.29411143972212]
-const swissAlps = [8.947253027980032,45.97757518134668]
+const placesList = document.getElementById('places-list');
+const placesListOverlay = document.getElementById('places-list-overlay');
+const title = document.getElementById('title');
+//const subtitle = document.getElementById('subtitle');
+
+//console.log(JSON.stringify(places.features))
+
+function loadPlace(feat) {
+    console.log(JSON.stringify(feat))
+
+    const { name } = feat.properties;
+    const [longitude, latitude] = feat.geometry.coordinates;
+    Procedural.displayLocation( { latitude, longitude } );
+    title.innerHTML = name;
+    //subtitle.innerHTML = `${height}m`;
+    placesListOverlay.classList.add( 'hidden' );
+}
+
+title.addEventListener( 'click', () => {
+    placesListOverlay.classList.remove( 'hidden' );
+} );
+
+places.features.map((feat, i) => {
+    console.log(JSON.stringify(feat))
+    const li = document.createElement( 'li' );
+    let p = document.createElement( 'p' );
+    p.innerHTML = feat.properties.name;
+    li.appendChild( p );
+    p = document.createElement( 'p' );
+    //p.innerHTML = i + 1;
+    li.appendChild( p );
+    placesList.appendChild( li );
+    li.addEventListener( 'click', () => loadPlace(feat));
+});
 
 const basemap_default = 'arc';
 var basemap = (new URLSearchParams(window.location.search)).get('basemap');
@@ -66,79 +85,8 @@ Procedural.setUserLocationControlVisible( true );
 Procedural.setRotationControlVisible( true );
 Procedural.setZoomControlVisible( true );
 
-flyTo(katoomba, function() {});
-
-function onClick(id, callback) {
-  document.getElementById(id).addEventListener('click', callback);
-}
-
-onClick('fly-to-kg', function() {
-  flyTo(kyrg, function() {});
-});
-
-onClick('fly-to-everest', function() {
-  flyTo(mtEverest, function() {});
-});
-
-onClick('fly-to-kilimanjaro', function() {
-  flyTo(mtKilimanjaro, function() {});
-});
-
-onClick('fly-to-katoomba', function() {
-  flyTo(katoomba, function() {});
-});
-
-onClick('fly-to-uluru', function() {
-  flyTo(uluru, function() {});
-});
-
-onClick('fly-to-denali', function() {
-  flyTo(mtDenali, function() {});
-});
-
-onClick('fly-to-cordillera', function() {
-  flyTo(cordilleraBlanca, function() {});
-});
-
-onClick('fly-to-grand-canyon', function() {
-  flyTo(grandCanyon, function() {});
-});
-
-onClick('fly-to-pik-pobedy', function() {
-  flyTo(pikPobedy, function() {});
-});
-
-onClick('fly-to-olympus', function() {
-  flyTo(mtOlympus, function() {});
-});
-
-onClick('fly-to-khan-tengri', function() {
-  flyTo(khanTengri, function() {});
-});
-
-onClick('fly-to-oahu', function() {
-  flyTo(oahuHawaii, function() {});
-});
-
-onClick('fly-to-fuji', function() {
-  flyTo(mtFuji, function() {});
-});
-
-onClick('fly-to-taranaki', function() {
-  flyTo(mtTaranaki, function() {});
-});
-
-onClick('fly-to-swissalps', function() {
-  flyTo(swissAlps, function() {});
-});
-
-
-function flyTo(location, done) {
-    Procedural.displayLocation( {
-        longitude: location[0],
-        latitude: location[1],
-        distance: 5000,
-    } );
+if (places.features.length > 0) {
+    loadPlace(places.features[0]);
 }
 
 navigator.geolocation.watchPosition(
